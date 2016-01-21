5b80f86bbb99909044b26858b314076ba2e7cf7e
Yi Wang <yi.wang.2005@gmail.com>
2016-01-21 09:28:01 -0800

    Use https://texlution.com/post/golang-canonical-import-paths/

--- topic.ai.go
+++ topic.ai.go
@@ -3,15 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+const (
+	goTagsTemplate = `<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="go-import" content="{{.ImportPath}} git {{.RepoPath}}">
+</head>
+<body>
+</body>
+</html>
+`
+)
+
+var (
+	temp *template.Template
+)
+
+type GoTagMap struct {
+	ImportPath string
+	RepoPath   string
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+
 	switch fmt.Sprint(r.URL) {
 	case "/weakand", "/weakand/":
-		http.Redirect(w, r, "https://github.com/wangkuiyi/weakand", http.StatusFound)
+		temp.Execute(w, GoTagMap{
+			ImportPath: "topic.ai/weakand",
+			RepoPath:   "https://github.com/wangkuiyi/weakand"})
 	case "/phoenix", "/phoenix/":
-		http.Redirect(w, r, "https://github.com/wangkuiyi/phoenix", http.StatusFound)
+		temp.Execute(w, GoTagMap{
+			ImportPath: "topic.ai/phoenix",
+			RepoPath:   "https://github.com/wangkuiyi/phoenix"})
 	default:
 		fmt.Fprintf(w, "Hello! Welcome to topic.ai!")
 	}
@@ -20,6 +50,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	addr := flag.String("addr", ":80", "Listening address")
 	flag.Parse()
+
+	if t, e := template.New("gotags").Parse(goTagsTemplate); e != nil {
+		log.Fatal(e)
+	} else {
+		temp = t
+	}
 
 	http.HandleFunc("/", viewHandler)
 	http.ListenAndServe(*addr, nil)
